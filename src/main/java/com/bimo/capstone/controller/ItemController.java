cac3af29533d02b7aa3c42f7d9f9e96564810782
aray.baimolda <baimolda.2001@gmail.com>
2022-11-27 17:05:12 +0600

    cors

--- src/main/java/com/bimo/capstone/controller/ItemController.java
+++ src/main/java/com/bimo/capstone/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.bimo.capstone.controller;
 import com.bimo.capstone.domain.StockItem;
 import com.bimo.capstone.repository.StockItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://epam-capstone.herokuapp.com/")
 @RequestMapping("/api/list")
 public class ItemController {
 
